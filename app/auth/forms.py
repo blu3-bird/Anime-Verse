a85878ad14e516d2b42dc022943612819bef2335
Arson <youaregifted@myyahoo.com>
2025-10-04 16:32:29 +0530

    Added login form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -29,3 +29,9 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError("Email already registed, Please use different one.")
+
+class LoginForm(FlaskForm):
+    username = StringField("Username",validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember_me = BooleanField("Remember Me")
+    submit = SubmitField("Sign In")
