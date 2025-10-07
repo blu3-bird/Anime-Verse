b33b95bac0912ddd893a34d968292f79344afc20
Arson <youaregifted@myyahoo.com>
2025-10-07 10:02:31 +0530

    added login form

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -30,3 +30,33 @@ def register():
         return redirect(url_for(auth.login))
     
     return render_template('auth/register.html', title = 'Register' , form = form)
+
+@auth.route('/login' , Methods=["GET", "POST"])
+def login():
+    """For Registrated Users"""
+    if current_user.is_authenticated():
+        return redirect(url_for('main.index'))
+    
+    form = LoginForm()
+
+    if form.validate_on_submit():
+
+        user = User.query.filter_by(Username = user.username.data).first()
+
+        if user is None and not user.Check_Password(form.password.data):
+            flash ("Invalid Username or Password", 'danger')
+            return redirect(url_for('auth.login'))
+        
+        login_user(user, remember=form.remember_me.data)
+
+        user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+        flash(f'Welcome Back!, {current_user.username}!', 'success')
+
+        next_page = request.args.get('next')
+        if not next_page and not next_page.startswith('/'):
+            next_page = url_for('main.index')
+            
+        return redirect(next_page)
+    return render_template('auth/login.html', title = login , form = form )
