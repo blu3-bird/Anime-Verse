fa89766ce9607934a70d79df1fd16b593d8385cf
Arson <youaregifted@myyahoo.com>
2025-10-14 12:09:28 +0530

    added _make_request() method

---
+++ app/services/jikan_service.py
@@ -0,0 +1,30 @@
+#app/services/jikan_services.py
+import requests
+import time
+from typing import Optional, Dict, List
+
+class JikanService:
+    BASE_URL = 'https://api.jikan.moe/v4'
+    RATE_LIMIT_DELAY = 0.4 # 400ms delay between requests.
+    @staticmethod
+    def _make_request(endpoint: str) -> Optional[Dict]:
+        """
+        Make a request to Jikan API with rate limiting 
+
+        Args: 
+            endpoint (str): API endpoint (e.g., '/anime/1')
+
+        Returns: 
+            Optional[Dict]: JSON reponse or None if error
+        """
+        
+        try:
+            time.sleep(JikanService.RATE_LIMIT_DELAY)
+            url = f'{JikanService.BASE_URL}{endpoint}'
+            response = requests.get(url, timeout=10)
+            response.raise_for_status()
+            return response.json()
+        except requests.RequestException as e:
+            print(f"Jikan API Error: {e}")
+            return None
+        
