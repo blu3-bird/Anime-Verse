7049434d82ab6b35c0461ce5427ae21f709e52b3
Arson <youaregifted@myyahoo.com>
2025-10-15 06:46:11 +0530

    added full functioning search bar

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,9 +1,25 @@
 # app/main/routes.py
-from flask import render_template, flash
+from flask import render_template, flash , request
+from flask_login import login_required , current_user
 from app.main import main
+from app.services.jikan_service import JikanService
 
 
 @main.route('/')
-@main.route('/index')
 def index():
-    return render_template('index.html' ,title= 'Home')
+    """Home page with search functionality"""
+    search_query = request.args.get('q', '').strip() #Get search query from URL parameters
+    search_results= []
+    if search_query:   #if user searched for something.
+        search_results = JikanService.search_anime(search_query)
+        if not search_results:
+            flash(f'No results found for "{search_query}"', 'info')
+    return render_template('index.html',
+                           search_query=search_query,
+                           search_results=search_results)
+
+@main.route('/watchlist')
+@login_required
+def watchlist():
+    """User's watchlist page"""
+    return render_template ('main/watchlist.html')
