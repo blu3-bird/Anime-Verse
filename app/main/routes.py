3c4cd9b3d7319d97e72e2f9b43d2fbcccd5e3ebe
Arson <youaregifted@myyahoo.com>
2025-10-16 15:12:44 +0530

    fixed back to search btn issue

--- app/main/routes.py
+++ app/main/routes.py
@@ -34,16 +34,23 @@ def watchlist():
     """User's watchlist page"""
     return render_template ('main/watchlist.html')
 
+
 @main.route('/anime/<int:anime_id>')
 def anime_details(anime_id):
     """Display detailed info bout a specific anime"""
 
-    search_query = request.args.get('q' , "").strip() #to get the searched keyword.
+    from_search = request.args.get('from_search' , "")
+    page = request.args.get('page', 1, type=int)
+    #to get the searched keyword.
 
     anime = JikanService.get_anime_details(anime_id)
 
     #if anime not found, show error
     if not anime:
-        flash(f'Anime with {search_query} not found', 'danger')
+        flash(f'Anime with {from_search} not found', 'danger')
         return redirect(url_for('main.index'))
-    return render_template('main/anime_details.html', anime=anime)
+    
+    return render_template('main/anime_details.html', 
+                           anime=anime,
+                           from_search=from_search,
+                           search_page=page)
